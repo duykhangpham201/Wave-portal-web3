2b80c4695861946aaff6743b1d80e9249cffde59
Kai Pham <duykhangpham201@gmail.com>
2021-12-31 19:12:26 -0500

    finalize

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/vhPQI0bWKLqy1fDMjrQcVjXdxZr3-6Mq",
-      accounts: ["bab5acecdbe53707ecbaa64e3675b952916ced259ba94fa3e09388c23d453345"],
+      url: PROD_ALCHEMY_KEY,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
